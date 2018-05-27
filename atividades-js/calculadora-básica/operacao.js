e7018933a3bac6abaa7af31b4dc07726382fadd3
Mateus Selani <selaneitor@gmail.com>
2018-05-27 19:16:52 -0300

    commit calculadora soma

---
+++ atividades-js/calculadora-básica/operacao.js
@@ -0,0 +1,16 @@
+function Calculo(){
+    let n1 = document.getElementById('primeiroCampo').value;
+    let n2 = document.getElementById('segundoCampo').value;
+
+    n1 = Number(n1);
+    n2 = Number(n2);
+
+    let n3 = n1 + n2;
+
+    document.getElementById('Resultado').value = n3;
+}
+function LimparCampos() {
+    document.getElementById('primeiroCampo').value = '';
+    document.getElementById('segundoCampo').value = '';
+    document.getElementById('Resultado').value = '';
+}
